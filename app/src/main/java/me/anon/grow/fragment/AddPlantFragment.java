6877e85cc159a69c8b3c442c43cbec8008dd8818
7LPdWcaW <7LPdWcaW@microgrowery>
2015-05-05 22:51:31 +0100

    Added basic plant details/add plant activity/fragment

---
+++ app/src/main/java/me/anon/grow/fragment/AddPlantFragment.java
@@ -0,0 +1,15 @@
+package me.anon.grow.fragment;
+
+import android.app.Fragment;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author Callum Taylor
+ * @documentation // TODO Reference flow doc
+ * @project GrowTracker
+ */
+public class AddPlantFragment extends Fragment
+{
+
+}
